fadc77d7316020856ffb235cd4c64113433e9996
Diego <diegoceto1017@gmail.com>
2021-09-04 16:13:12 -0500

    Fichero condicionales

---
+++ condicionales.py
@@ -0,0 +1,12 @@
+usu_aLmacenado='diego-sena'
+clave_almacenada='misiontic2022'
+
+usuario=input('Ingesa tu nombre de usuario:')
+clave=input("Ingresa la contraseña:")
+
+if usuario==usu_aLmacenado or clave==clave_almacenada:
+    print("Ingreso exitoso")
+    
+else:
+    print("Su usuario y contraseña estan incorrectas")
+    
